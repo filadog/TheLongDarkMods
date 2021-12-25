47d5d92d3c758f979ad8f20569f4dbc805119bca
filadog <filimonov1995@gmail.com>
2021-12-25 10:50:32 +0300

    Mod setup

---
+++ DemoMod/DemoMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace DemoMod
+{
+    public class DemoMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log("DemoMod by filadog initialized");
+        }
+    }
+}
---
+++ DemoMod/Patch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DemoMod
+{
+    internal class Patch
+    {
+        
+    }
+
+    [HarmonyPatch(typeof(GameManager), "Awake")]
+    internal class GameManager_Awake
+    {
+        private static void Postfix()
+        {
+            MelonLoader.MelonLogger.Msg("Hello");
+        }
+    }
+}
